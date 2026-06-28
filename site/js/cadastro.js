e18f45f248debd600ae27d055f25b19d7ec00658
Trigger <triggerww@outlook.com>
2026-06-28 18:57:48 -0300

    cadastro de pets localstorage

--- site/js/cadastro.js
+++ site/js/cadastro.js
@@ -1,21 +1,32 @@
-const botao = document.getElementById('cadastrar');
 const formulario = document.getElementById('cadastro');
 const fundo = document.getElementById('fundo');
 
 function mostrar() {
-    if (formulario.style.display === 'none' || formulario.style.display === '') {
-        formulario.style.display = 'block';
-        fundo.style.display = 'block';
-    } 
+    formulario.style.display = 'block';
+    fundo.style.display = 'block';
 }
 
-botao.addEventListener('click', mostrar);
-
-function fechar(event) {
-    if(event.target == fundo) {
-        formulario.style.display = 'none';
-        fundo.style.display = 'none';
-    }
+function ocultar() {
+    formulario.style.display = 'none';
+    fundo.style.display = 'none';
 }
 
-fundo.addEventListener('click', fechar)
+document.getElementById('cadastrar').addEventListener('click', mostrar);
+document.getElementById('btn-cancelar').addEventListener('click', ocultar);
+
+function registrar_pet() {
+    const nome = document.getElementById('nome').value;
+    const especie = document.getElementById('especie').value;
+    const raca = document.getElementById('raca').value;
+    const idade = document.getElementById('idade').value;
+    const contato = document.getElementById('contato').value; 
+    const imagem = document.getElementById('url').value;
+
+    const pet = [
+        nome, especie, raca, idade, contato, imagem
+    ];
+
+    localStorage.setItem(nome, JSON.stringify(pet));
+}
+
+document.getElementById('btn-cadastro').addEventListener('click', registrar_pet);
